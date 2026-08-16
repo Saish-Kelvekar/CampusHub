8032b201724f72c1caec271cd1d044b65db8974b
Saish-Kelvekar <kelvekarsaish@gmail.com>
2026-08-16 22:28:42 +0530

    Connect CampusHub REST API to MYSQL

--- backend/server.js
+++ backend/server.js
@@ -1,185 +1,316 @@
 const express = require("express");
-const cors=require("cors")
-const app=express();
+const mysql = require("mysql2/promise");
+const cors = require("cors")
+const app = express();
 app.use(cors());
 app.use(express.json());//used for post reason  JSON---(parse)-->req.body
-const PORT =3000;
+const PORT = 3000;
 
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "campushub"
+});
 
-app.get("/",(req,res)=>{//syntax (path,callback)
+async function testDatabase() {
+    try {
+        const [result] = await db.query("SELECT 1");
+        console.log("MYSQL connected successfully", result);
+    } catch (error) {
+        console.error("MySql connection failed", error);
+    }
+}
+testDatabase();
+
+app.get("/", (req, res) => {//syntax (path,callback)
     res.send("welcome to CampusHub");
 })
 
 
-const announcements=[
-    {
-        id: 1,
-        title: "End Semester Examination",
-        date: "August 20",
-        description: "End semester examination timetable has been released."
-    },
-    {
-        id: 2,
-        title: "Hackathon Registration",
-        date: "August 25",
-        description: "Registrations are open for the upcoming campus hackathon."
-    }
-];
+// const announcements=[
+//     {
+//         id: 1,
+//         title: "End Semester Examination",
+//         date: "August 20",
+//         description: "End semester examination timetable has been released."
+//     },
+//     {
+//         id: 2,
+//         title: "Hackathon Registration",
+//         date: "August 25",
+//         description: "Registrations are open for the upcoming campus hackathon."
+//     }
+// ];
 
-app.get("/api/announcements",(req,res)=>{
-    res.json(announcements);
-})
+app.get("/api/announcements", async (req, res) => {
+    try {
+        const [rows] = await db.query(
+            "SELECT * FROM ANNOUNCEMENTS"
+        );
 
-app.post("/api/announcements",(req,res)=>{
-    const newAnnouncement={
-        id:announcements.length+1,
-        title:req.body.title,
-        date:req.body.date,
-        description:req.body.description
-    };
+        res.json(rows);
+    } catch (error) {
+        console.error("Error fetching announcements: ", error);
 
-    announcements.push(newAnnouncement);
-    res.status(201).json(newAnnouncement);
-});
-
-
-app.delete("/api/announcements/:id",(req,res)=>{
-    const id=Number(req.params.id);
-
-    const index=announcements.findIndex(
-        (item)=>item.id=== id
-    );
-    if(index===-1){
-        res.status(404).json({
-            message:"Announcement not found"
+        res.status(500).json({
+            message: "Failed to fetch announcements"
         });
     }
+});
 
-    announcements.splice(index,1);
-    res.json({
-        message:"Announcement deleted successfully"
-    });
+app.post("/api/announcements", async (req, res) => {
+    // const newAnnouncement={
+    //     id:announcements.length+1,
+    //     title:req.body.title,
+    //     date:req.body.date,
+    //     description:req.body.description
+    // };
+
+    // announcements.push(newAnnouncement);
+    // res.status(201).json(newAnnouncement);
+
+    try {
+        const { title, date, description } = req.body;
+
+        const [result] = await db.query(
+            `INSERT INTO ANNOUNCEMENTS (title,date,description)
+            VALUES(?,?,?)`,
+            [title, date, description]
+        );
+
+        const [rows] = await db.query(
+            "SELECT * FROM ANNOUNCEMENTS WHERE ID=?", [result.insertId]
+        );
+        res.status(201).json(rows[0]);
+    } catch (error) {
+        console.error("Error adding announcements ", error);
+        res.status(500).json({
+            message: "Failed to add annoucement"
+        });
+    }
 });
-app.listen(PORT,()=>{//(platform,callback)
-    console.log(`CampusHub server running on port ${PORT}`);
+
+
+app.delete("/api/announcements/:id", async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+
+        const [result] = await db.query(
+            "DELETE FROM ANNOUNCEMENTS WHERE ID=?", [id]
+        );
+        if (result.affectedRows === 0) {
+            return res.status(404).json({
+                message: "Announcement not found"
+            });
+        }
+
+
+        res.json({
+            message: "Announcement deleted successfully"
+        });
+    } catch (error) {
+        console.error("Error deleting announcements:", error);
+        res.status(500).json({
+            message: "Failed to delete announcement"
+        });
+    }
 });
+
 
 
 
 /*========EVENTS API==========*/
 
-const events=[
-    {
-        id:1,
-        title:"Campus Hackathon",
-        date : "August 18",
-        location:"Main Auditorium"
-    },
-    {
-        id: 2,
-        title: "Technical Workshop",
-        date: "August 22",
-        location: "Computer Lab 2"
-    },
-    {
-        id: 3,
-        title: "Sports Meet",
-        date: "August 27",
-        location: "College Ground"
+// const events=[
+//     {
+//         id:1,
+//         title:"Campus Hackathon",
+//         date : "August 18",
+//         location:"Main Auditorium"
+//     },
+//     {
+//         id: 2,
+//         title: "Technical Workshop",
+//         date: "August 22",
+//         location: "Computer Lab 2"
+//     },
+//     {
+//         id: 3,
+//         title: "Sports Meet",
+//         date: "August 27",
+//         location: "College Ground"
+//     }
+// ];
+
+app.get("/api/events", async (req, res) => {
+    try {
+        const [rows] = await db.query(
+            "SELECT * FROM EVENTS"
+        );
+        res.json(rows);
+    } catch (error) {
+        console.error("Error fetching events: ", error);
+        res.status(500).json({
+            message: "Failed to fetch events"
+        });
     }
-];
-
-app.get("/api/events",(req,res)=>{
-    res.json(events);
 });
 
 
-app.post("/api/events",(req,res)=>{
-    const newEvent={
-        id: events.length + 1,
-        title: req.body.title,
-        date: req.body.date,
-        location: req.body.location
-    };
-    events.push(newEvent);
-    res.status(201).json(newEvent);
+app.post("/api/events", async (req, res) => {
+    // const newEvent={
+    //     id: events.length + 1,
+    //     title: req.body.title,
+    //     date: req.body.date,
+    //     location: req.body.location
+    // };
+    // events.push(newEvent);
+    // res.status(201).json(newEvent);
+
+    try {
+        const { title, date, location } = req.body;
+        const [result] = await db.query(
+            `INSERT INTO EVENTS (TITLE,DATE,LOCATION)
+            VALUES(?,?,?)`, [title, date, location]
+        );
+
+        const [rows] = await db.query(
+            "SELECT * FROM EVENTS WHERE id=?", [result.insertId]
+        );
+
+        res.status(201).json(rows[0]);
+
+    } catch (error) {
+        console.error("Error adding event:", error);
+
+        res.status(500).json({
+            message: "Failed to add event"
+        });
+    }
 });
 
 
-app.delete("/api/events",(req,res)=>{
-    const id=Number(req.params.id);
+app.delete("/api/events/:id", async (req, res) => {
+    try {
+        const id = Number(req.params.id);
 
-    const index=events.findIndex(
-        (item)=>item.id===id
-    );
+        const [result] = await db.query(
+            "DELETE FROM EVENTS WHERE id=?", [id]
+        );
 
-    if(index===-1){
-        return res.status(404).json({
-            message:"Event not found"
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({
+                message: "Event not found"
+            });
+        }
+
+
+
+        res.json({
+            message: "Event deleted successfully"
+        });
+    } catch (error) {
+        console.error("Error deleting event :", error);
+        res.status(500).json({
+            message: "Failed to delete event"
+        });
+    }
+});
+
+
+// const notes = [
+//     {
+//         id: 1,
+//         title: "Data Structures",
+//         subject: "Computer Science",
+//         description: "Linked lists, stacks, queues and trees."
+//     },
+//     {
+//         id: 2,
+//         title: "Operating Systems",
+//         subject: "Computer Science",
+//         description: "CPU scheduling and process management."
+//     },
+//     {
+//         id: 3,
+//         title: "Machine Learning",
+//         subject: "Artificial Intelligence",
+//         description: "Regression, classification and model evaluation."
+//     }
+// ];
+
+app.get("/api/notes", async (req, res) => {
+    try {
+        const [result] = await db.query(
+            "SELECT * FROM NOTES"
+        );
+        res.json(result);
+    }
+    catch (error) {
+        console.error("Error in fetching notes: ", error);
+
+        res.status(500).json({
+            message: "Failed fetching notes"
+        })
+    }
+});
+
+app.post("/api/notes", async (req, res) => {
+    try {
+        const { title, subject, description } = req.body;
+        const [result] = await db.query(
+            `INSERT INTO NOTES (TITLE,SUBJECT,DESCRIPTION)
+            VALUES(?,?,?)`, [title, subject, description]
+
+        );
+
+        const [rows] = await db.query(
+            "SELECT * FROM NOTES WHERE ID =?", [result.insertId]
+        );
+
+        res.status(201).json(rows[0]);
+    }
+    catch (error) {
+        console.error("Error adding note:", error);
+
+        res.status(500).json({
+            message: "Failed to add note"
         });
     }
 
-    events.splice(index,1);
 
-    res.json({
-        message:"Event deleted successfully"
-    });
 });
 
+app.delete("/api/notes/:id", async (req, res) => {
+    try {
+        const id = Number(req.params.id);
 
-const notes = [
-    {
-        id: 1,
-        title: "Data Structures",
-        subject: "Computer Science",
-        description: "Linked lists, stacks, queues and trees."
-    },
-    {
-        id: 2,
-        title: "Operating Systems",
-        subject: "Computer Science",
-        description: "CPU scheduling and process management."
-    },
-    {
-        id: 3,
-        title: "Machine Learning",
-        subject: "Artificial Intelligence",
-        description: "Regression, classification and model evaluation."
-    }
-];
+        const [result] = await db.query(
+            "DELETE FROM NOTES WHERE ID=?", [id]
+        );
 
-app.get("/api/notes",(req,res)=>{
-    res.json(notes);
-});
+        if (result.affectedRows === 0) {
+            return res.status(404).json({
+                message: "Note not found"
+            });
+        }
 
-app.post("/api/notes",(req,res)=>{
-    const newNotes={
-        id:notes.length+1,
-        title:req.body.title,
-        subject:req.body.subject,
-        description:req.body.description
-    };
 
-    notes.push(newNotes);
-    res.status(201).json(newNotes);
-});
 
-app.delete("/api/notes",(req,res)=>{
-    const id=Number(req.body.id);
+        res.json({
+            message: "Notes deleted successfully"
+        });
+    } catch (error) {
+        console.error("Error deleting note :", error);
 
-    const index=notes.findIndex(
-        (items)=> items.id===id
-    );
-
-    if(index===-1){
-        return res.status(404).json({
-            message:"Note not found"
+        res.status(500).json({
+            message: "Failed to delete note"
         });
     }
+});
 
-    notes.splice(index,1);
-
-    res.json({
-        message:"Notes deleted successfully"
-    });
+app.listen(PORT, () => {//(platform,callback)
+    console.log(`CampusHub server running on port ${PORT}`);
 });
